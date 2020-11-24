1e6204ae0343f723931e339864eed34de0e097de
Decker <56712123+Alex-Decker@users.noreply.github.com>
2020-11-24 14:26:20 +0100

    Update app.php
    tyfuy

--- web/app.php
+++ web/app.php
@@ -15,4 +15,4 @@ $kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
-$kernel->terminate($request, $response);
+$kernel->terminate($request, $response);//dfgfdhtfuyj
